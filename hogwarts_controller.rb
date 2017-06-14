51c9be60a77bafc00ee0bfb4991b09c4622d695c
James Cox <j-cox03@hotmail.com>
2017-06-14 18:43:58 +0100

    Added dynamic input of house names in new.erb. Added student name to create.erb

--- hogwarts_controller.rb
+++ hogwarts_controller.rb
@@ -5,25 +5,19 @@ require('pry-byebug')
 require_relative('models/student.rb')
 require_relative('models/house.rb')
 
-# INDEX action 3 of 7.
+# INDEX
 get "/students" do
   @students = Student.all()
   erb(:index)
 end
 
-# NEW action 1 of 7. Must be above the SHOW route!
-# There is a link in layout.erb that takes you to the Order form.
+# NEW
 get "/students/new" do
+  @houses = House.all()
   erb(:new)
 end
 
-# # SHOW action 4 of 7.
-# get "/students/:id" do
-#   @student = Student.find(params["id"].to_i)
-#   erb(:show)
-# end
-
-# CREATE action 2 of 7.
+# CREATE
 post "/students" do
   @student = Student.new(params)
   @student.save()
